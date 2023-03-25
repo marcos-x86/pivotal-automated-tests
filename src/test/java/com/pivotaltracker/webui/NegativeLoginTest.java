8befbd586c7c1b1c9fb6c9470e2379437719768c
ivisrosales <ivisrosalesvargas@gmail.com>
2023-03-25 16:33:27 -0400

    Modify src/test/java/com/pivotaltracker/webui/ProjectNegativeTests

--- src/test/java/com/pivotaltracker/webui/NegativeLoginTest.java
+++ src/test/java/com/pivotaltracker/webui/NegativeLoginTest.java
@@ -35,3 +35,6 @@ public class NegativeLoginTest {
 
     }
 }
+
+                
+                //prueba2
--- src/test/java/com/pivotaltracker/webui/ProjectNegativeTests.java
+++ src/test/java/com/pivotaltracker/webui/ProjectNegativeTests.java
@@ -50,6 +50,8 @@ public class ProjectNegativeTests {
             }
 
             currentExecution++;
+
+            //prueba
         }
 
     }
